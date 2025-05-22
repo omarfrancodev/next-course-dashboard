fb3be563f5b1f953bbe0f090c08471da1f6a2dd3
Omar Franco <desarrollo.software2@prosur.com.mx>
2025-05-22 17:55:00 -0600

    feat: configurate the creating of static pages at build time based on a static page by fetching the data on build time

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,29 +1,10 @@
-import { PokemonsGrid, PokemonsResponse, SimplePokemon } from "@/pokemons";
+import { getPokemons, PokemonsGrid, SimplePokemon } from "@/pokemons";
 
 
 export const metadata = {
     title: 'Pokemons List',
     description: 'Pokemons List',
 };
-
-const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
-    const dataResponse: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`, {
-        cache: 'force-cache',
-        next: {
-            revalidate: 60
-        }
-    })
-        .then(res => res.json());
-
-    const pokemons: SimplePokemon[] = dataResponse.results.map(pokemon => ({
-        id: pokemon.url.split('/').at(-2)!,
-        name: pokemon.name
-    }));
-
-    // throw new Error('Error al obtener los pokemons');
-
-    return pokemons;
-}
 
 
 export default async function PokemonsPage() {
