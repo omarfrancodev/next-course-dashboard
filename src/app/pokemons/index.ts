18a395f542df4c7ffafcbfedeac76af5b4e3448a
Omar Franco <desarrollo.software2@prosur.com.mx>
2025-05-22 11:10:49 -0600

    feat(pokemons): extract grid elements and pokemon cards into resuable components with data retrieval on the pokemon page

---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,8 @@
+
+// Interfaces
+export type { PokemonsResponse } from './interfaces/pokemons-response';
+export type { SimplePokemon } from './interfaces/simple-pokemon';
+
+// Components
+export { PokemonsGrid } from './components/PokemonsGrid'
+export { PokemonCard } from './components/PokemonCard';
