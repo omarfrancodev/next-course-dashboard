0eb98aeb9aa27e60d1a4d5067a12dff86355cfc8
omarfrancodev <fofe2803@gmail.com>
2025-05-22 16:04:49 -0600

    refactor: remve global padding in dashboard layout and adjust pages to use its own padding style

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function CounterPage() {
 
     return (
-        <div className="flex flex-col items-center justify-center w-full h-full">
+        <div className="p-4 flex flex-col items-center justify-center w-full h-full">
             <span> Productos en el carrito: </span>
             <CartCounter value={10} />
         </div>
--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -30,7 +30,7 @@ export default async function PokemonsPage() {
 
     const pokemons: SimplePokemon[] = await getPokemons(200);
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="flex flex-col items-center justify-center min-h-screen p-4">
             <span className="text-4xl font-bold">Listado de Pokémons <small>estático</small></span>
             <PokemonsGrid pokemons={pokemons} />
         </div >
