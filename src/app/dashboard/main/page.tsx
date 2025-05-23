e40c21e4ed31a4b363efeedfeaabfee34b1027f0
omarfrancodev <fofe2803@gmail.com>
2025-05-23 14:16:29 -0600

    feat: adding new component and start modifyng main page, create basic new page for favorites pokemons

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,8 +1,14 @@
+import { SimpleWidget } from "@/components";
 
 export default function MainPage() {
     return (
-        <div>
-            <h1>Hello Page</h1>
+        <div className="text-black p-4">
+            <h1 className="text-3xl font-semibold">Dashboard</h1>
+            <span className="text-xl">Información general</span>
+
+            <div className="flex flex-wrap p-2">
+                <SimpleWidget />
+            </div>
         </div>
     );
 }
