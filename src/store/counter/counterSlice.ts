f990bca767447657ecafe57ca1e0bf9f8aea8561
Omar Franco <desarrollo.software2@prosur.com.mx>
2025-05-23 15:55:08 -0600

    feat: implementing and configuring redux for the counter functionality on the counter page

---
+++ src/store/counter/counterSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface CounterState {
+    count: number;
+}
+
+const initialState: CounterState = {
+    count: 0
+}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increaseCount: (state) => {
+            state.count++;
+        },
+        decreaseCount: (state) => {
+            if (state.count === 0) return;
+            state.count--;
+        },
+        resetCountTo: (state, action: PayloadAction<number>) => {
+            if (action.payload < 0) action.payload = 0;
+            state.count = action.payload;
+        }
+    }
+});
+
+export const { increaseCount, decreaseCount, resetCountTo } = counterSlice.actions
+
+export default counterSlice.reducer
